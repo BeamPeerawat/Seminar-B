2efdbb23abb686381313532f4ac6464c606bd51e
BeamCSNV <143855345+BeamCSNV@users.noreply.github.com>
2024-12-02 01:38:28 +0700

    02/12/2567 1:38

--- server.js
+++ server.js
@@ -1,27 +1,33 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const quotationRoutes = require('./routes/quotationRoutes');
-require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const quotationRoutes = require("./routes/quotationRoutes");
+const authRoutes = require("./routes/auth");
+const lineAuthRoutes = require("./routes/lineAuth");
+require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(bodyParser.json());
-app.use(cors({ origin: 'http://localhost:3000' })); // ตั้งค่า CORS ให้รับคำขอจาก localhost:3000 หรือ URL ที่เว็บไซต์ของคุณใช้งานอยู่
-app.use('/quotations', quotationRoutes);
-
+app.use(cors({ origin: "http://localhost:3000" })); // ตั้งค่า CORS ให้รับคำขอจาก localhost:3000 หรือ URL ที่เว็บไซต์ของคุณใช้งานอยู่
+app.use("/quotations", quotationRoutes);
+app.use("/auth", authRoutes);
+app.use("/", lineAuthRoutes);
 // MongoDB Connection
 mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB connected'))
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log(err));
 
 // Routes
-app.get('/', (req, res) => {
-  res.send('API is running...');
+app.get("/", (req, res) => {
+  res.send("API is running...");
 });
 
 // Start Server
