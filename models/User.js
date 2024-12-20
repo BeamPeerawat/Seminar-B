8ce04813d322d4c941f2d6a9f648125fd76993a1
BeamCSNV <143855345+BeamCSNV@users.noreply.github.com>
2024-12-20 23:28:44 +0700

    Initial commit

---
+++ models/User.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const UserSchema = new mongoose.Schema(
+  {
+    fullname: { type: String, required: true },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, "Please enter a valid email address"],
+    },
+    password: { type: String, required: true },
+  },
+  { timestamps: true } // เปิดใช้งาน timestamps
+);
+
+// เข้ารหัส Password ก่อนบันทึก
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next(); // ตรวจสอบว่ารหัสผ่านมีการเปลี่ยนแปลงหรือไม่
+  const saltRounds = 10;
+  this.password = await bcrypt.hash(this.password, saltRounds);
+  next();
+});
+
+module.exports = mongoose.model("User", UserSchema);
