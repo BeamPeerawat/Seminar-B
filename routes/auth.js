1ec735aa308302bb3877bcddbd138e86705e5b4a
BeamCSNV <143855345+BeamCSNV@users.noreply.github.com>
2024-12-20 07:20:05 +0700

    Initial commit

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 // routes/auth.js
 const express = require("express");
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const User = require("../models/User");
 const router = express.Router();
 
