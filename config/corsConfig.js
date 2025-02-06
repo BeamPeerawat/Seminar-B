4f53c4580959b8e54489375d326a87c84cf6bcb3
BeamCSNV <143855345+BeamCSNV@users.noreply.github.com>
2025-02-07 02:02:09 +0700

    Last

--- config/corsConfig.js
+++ config/corsConfig.js
@@ -2,7 +2,8 @@
 
 export const corsOptions = {
   origin: [
-    process.env.CLIENT_URL || "http://localhost:3000", // Frontend domain
+    process.env.CLIENT_URL || "http://localhost:3000", // ใช้ URL ของ Frontend ในเครื่อง
+    "https://seminar-f.vercel.app", // เพิ่ม URL ของ Frontend บน Vercel
     "http://another-frontend-domain.com", // ตัวอย่างอนุญาต domain อื่นๆ
   ],
   methods: "GET,POST", // กำหนด HTTP methods ที่อนุญาต
