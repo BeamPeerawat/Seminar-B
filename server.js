43b0ce42fa5c86b4011dcbe15fcc8286a0daea8c
BeamCSNV <143855345+BeamCSNV@users.noreply.github.com>
2025-02-06 23:38:58 +0700

    Fix: Allow app to listen on external IP

--- server.js
+++ server.js
@@ -40,6 +40,7 @@ connectDB()
   .catch((error) => logger.error("Failed to connect to MongoDB:", error));
 
 // Start Server
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   logger.info(`Server is running on http://localhost:${PORT}`);
 });
+
