770ff31e88ea5d539cb1e1c61077460829daaebe
BeamCSNV <143855345+BeamCSNV@users.noreply.github.com>
2025-02-20 04:43:25 +0700

    รับรู้บัญชี LINE หน้าบริการ แก้ไข 4.43

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -2,26 +2,20 @@ import jwt from "jsonwebtoken";
 import User from "../models/User.js";
 
 const authenticate = async (req, res, next) => {
-  // ดึง Token จาก Cookies หรือ Authorization Header
-  const token =
-    req.cookies.authToken || req.headers.authorization?.split(" ")[1];
+  const token = req.cookies.authToken || req.headers.authorization?.split(" ")[1];
 
   if (!token) {
     return res.status(401).json({ success: false, message: "Unauthorized: No token provided" });
   }
 
   try {
-    // ตรวจสอบ Token
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-    // ค้นหาผู้ใช้จากฐานข้อมูล
-    const user = await User.findById(decoded.id);
+    const user = await User.findById(decoded.userId);
 
     if (!user) {
       return res.status(401).json({ success: false, message: "Unauthorized: User not found" });
     }
 
-    // เพิ่มข้อมูลผู้ใช้ใน Request Object
     req.user = user;
     next();
   } catch (error) {
