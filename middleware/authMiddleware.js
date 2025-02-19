9b676e2048d19f1eac4ffa8effedb19f1accd98b
BeamCSNV <143855345+BeamCSNV@users.noreply.github.com>
2025-02-19 18:55:29 +0700

    Last ตอนนี้

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,17 +1,8 @@
 // middleware/authMiddleware.js
-export const verifyToken = (req, res, next) => {
-  // ตรวจสอบ Token จาก header หรือ request
-  const token = req.headers["authorization"];
-  if (!token) {
-    return res.status(403).json({ message: "No token provided" });
-  }
 
-  try {
-    // ตรวจสอบ token เช่นกับ JWT
-    const decoded = jwt.verify(token, process.env.JWT_SECRET); // ใช้ JWT เพื่อ decode
-    req.user = decoded; // เก็บข้อมูลของ user ที่ได้จาก token
-    next();
-  } catch (error) {
-    return res.status(401).json({ message: "Invalid token" });
+export const authMiddleware = (req, res, next) => {
+  if (!req.user) {
+    return res.status(401).json({ message: "Unauthorized" });
   }
+  next();
 };
