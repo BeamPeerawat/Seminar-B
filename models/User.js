d982574c782d35405e0223ea49a0fe9195d949e9
BeamCSNV <143855345+BeamCSNV@users.noreply.github.com>
2025-02-22 16:39:11 +0700

    Last 16.39

--- models/User.js
+++ models/User.js
@@ -2,15 +2,14 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
   userId: { type: String, required: true, unique: true }, // ใช้ userId เป็น unique identifier
-  displayName: { type: String, required: false, default: "Anonymous" }, // Optional, default เป็น "Anonymous"
-  fullname: { type: String, required: false, default: "Anonymous" }, // Optional, default เป็น "Anonymous"
+  displayName: { type: String, required: false, default: "Anonymous" }, // Optional
+  fullname: { type: String, required: false, default: "Anonymous" }, // Optional
   pictureUrl: { type: String, required: false }, // Optional
   statusMessage: { type: String, required: false }, // Optional
-  email: { type: String, required: false }, // Optional, not unique (รองรับ null และซ้ำ)
   role: { type: String, enum: ["user", "admin"], default: "user" }, // Default role เป็น "user"
   profileCompleted: { type: Boolean, default: false }, // ใช้เช็คว่าข้อมูลโปรไฟล์สมบูรณ์หรือไม่
-  createdAt: { type: Date, default: Date.now }, // เพิ่ม timestamp เพื่อติดตามการสร้างผู้ใช้
-  updatedAt: { type: Date, default: Date.now }, // เพิ่ม timestamp เพื่อติดตามการอัปเดต
+  createdAt: { type: Date, default: Date.now }, // เพิ่ม timestamp
+  updatedAt: { type: Date, default: Date.now }, // เพิ่ม timestamp
 });
 
 userSchema.pre("save", function (next) {
