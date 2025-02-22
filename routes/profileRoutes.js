22df2b3f91be3e75443daf577766e2b1ded446ff
BeamCSNV <143855345+BeamCSNV@users.noreply.github.com>
2025-02-22 15:51:33 +0700

    Last 15.51

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -4,6 +4,7 @@ import {
   saveProfile,
   getProfileFromDB,
 } from "../controllers/profileController.js"; // เพิ่มการนำเข้า getProfileFromDB
+import { updateProfileCompleted } from "../controllers/authController.js"; // นำเข้า updateProfileCompleted จาก authController
 
 const router = express.Router();
 
@@ -11,5 +12,6 @@ const router = express.Router();
 router.post("/check-profile", checkProfile); // ตรวจสอบโปรไฟล์
 router.post("/save-profile", saveProfile); // บันทึกโปรไฟล์
 router.post("/get-profile-from-db", getProfileFromDB); // ดึงข้อมูลโปรไฟล์จากฐานข้อมูล
+router.post("/update-profile-completed", updateProfileCompleted); // อัปเดตสถานะ profileCompleted
 
 export default router;
