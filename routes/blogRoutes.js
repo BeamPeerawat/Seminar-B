27d64fba0085752f3ef534f0674329cc8a2b2a52
BeamCSNV <143855345+BeamCSNV@users.noreply.github.com>
2025-03-15 04:12:24 +0700

    5

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -19,13 +19,9 @@ router.post("/add", async (req, res) => {
 router.get("/", async (req, res) => {
   try {
     const blogs = await Blog.find();
-    if (!blogs) {
-      return res.status(404).json({ message: "No blogs found" });
-    }
     res.status(200).json(blogs);
   } catch (error) {
-    logger.error("Error fetching blogs:", error); // ใช้ logger
-    res.status(500).json({ message: "Internal server error", error: error.message });
+    res.status(500).json({ message: "Error fetching blogs", error });
   }
 });
 
