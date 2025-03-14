7ce1149485d961bb7e5c2b2a41bc05ce90edebf9
BeamCSNV <143855345+BeamCSNV@users.noreply.github.com>
2025-03-14 23:55:40 +0700

    3

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,14 +1,7 @@
-// // middleware/authMiddleware.js
-// export const authMiddleware = (req, res, next) => {
-//   if (!req.user || !req.user.userId) { // ตรวจสอบว่า req.user และ userId มีค่า
-//     return res.status(401).json({ message: "Unauthorized" });
-//   }
-//   next();
-// };
-export const authMiddleware = async (req, res, next) => {
-  const user = await User.findById(req.userId); // สมมติว่า req.userId มาจาก token
-  if (!user || user.role !== "admin") {
-    return res.status(403).json({ message: "Admin access required" });
+// middleware/authMiddleware.js
+export const authMiddleware = (req, res, next) => {
+  if (!req.user || !req.user.userId) { // ตรวจสอบว่า req.user และ userId มีค่า
+    return res.status(401).json({ message: "Unauthorized" });
   }
   next();
 };
