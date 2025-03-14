c1d54f66554a84980e342b6ce741a28d210022de
BeamCSNV <143855345+BeamCSNV@users.noreply.github.com>
2025-03-14 23:57:42 +0700

    4

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -90,13 +90,4 @@ router.get("/profile/:userId", async (req, res) => {
   }
 });
 
-router.get("/", async (req, res) => {
-  try {
-    const users = await User.find();
-    res.status(200).json(users);
-  } catch (error) {
-    res.status(500).json({ message: "Error fetching users", error });
-  }
-});
-
 export default router;
