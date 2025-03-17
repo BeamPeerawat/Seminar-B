17e0831864cf2b9823fd38ce3b9704f70532c893
BeamCSNV <143855345+BeamCSNV@users.noreply.github.com>
2025-03-17 10:43:22 +0700

    ปรับปรุงการบริการ(สินค้า)5

---
+++ utils/serviceUtils.js
@@ -0,0 +1,17 @@
+export const getProductIdsByService = (serviceId) => {
+    const serviceMap = {
+      "solar-panel": [1, 2, 3],
+      "solar-tank": [4, 5],
+      "well-drilling": [6, 7],
+    };
+    return serviceMap[serviceId] || [];
+  };
+  
+  export const getServiceTitle = (serviceId) => {
+    const serviceTitles = {
+      "solar-panel": "โซลาร์เซลล์",
+      "solar-tank": "หอถังสูงโซลาร์เซลล์",
+      "well-drilling": "เจาะบาดาลระบบแอร์",
+    };
+    return serviceTitles[serviceId] || "ไม่ระบุ";
+  };
