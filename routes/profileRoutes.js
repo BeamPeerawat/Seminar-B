2ddf31177d4b03cb38cd47327dcd81c8bdcdfa57
BeamCSNV <143855345+BeamCSNV@users.noreply.github.com>
2025-03-17 11:17:45 +0700

    ปรับปรุงการบริการ(สินค้า)7

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -5,13 +5,13 @@ import {
   getProfileFromDB,
 } from "../controllers/profileController.js";
 import { updateProfileCompleted } from "../controllers/authController.js";
-import { authenticateToken } from "../middleware/authenticate.js"; // เพิ่มการนำเข้า
+import authenticate from "../middleware/authenticate.js"; // เปลี่ยนเป็น authenticate
 
 const router = express.Router();
 
-router.post("/check-profile", authenticateToken, checkProfile); // ใช้ middleware
-router.post("/save-profile", authenticateToken, saveProfile); // ใช้ middleware
-router.post("/get-profile-from-db", authenticateToken, getProfileFromDB); // ใช้ middleware
-router.post("/update-profile-completed", authenticateToken, updateProfileCompleted); // ใช้ middleware
+router.post("/check-profile", authenticate, checkProfile); // ใช้ middleware
+router.post("/save-profile", authenticate, saveProfile); // ใช้ middleware
+router.post("/get-profile-from-db", authenticate, getProfileFromDB); // ใช้ middleware
+router.post("/update-profile-completed", authenticate, updateProfileCompleted); // ใช้ middleware
 
 export default router;
