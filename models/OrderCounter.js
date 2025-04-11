a936ffa8c5a69bc5604f74f0e0c79bfff3ab59d4
BeamCSNV <beamsaenpong@gmail.com>
2025-04-11 23:51:31 +0700

    Fix Change objectId Oder to Number1

--- models/OrderCounter.js
+++ models/OrderCounter.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const orderCounterSchema = new mongoose.Schema({
   _id: { type: String, required: true, default: "order_counter" },
-  sequence_value: { type: Number, default: 10000 },
+  sequence_value: { type: Number, default: 9999 }, // เปลี่ยนจาก 10000 เป็น 9999 เพราะจะ +1 ตอนใช้งาน
 });
 
 const OrderCounter = mongoose.models.OrderCounter || mongoose.model("OrderCounter", orderCounterSchema);
